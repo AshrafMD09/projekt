e242a1a9264af4d02faf2299b5abbc53ce4b26bb
Shlayten <jaouane.mohamed.achraf@gmail.com>
2024-02-18 21:42:41 +0100

    First touch :)

---
+++ app.js
@@ -0,0 +1,16 @@
+let inputTxt = document.querySelector(".inputTxt");
+let subBtn = document.querySelector(".subBtn");
+let resetBtn = document.querySelector(".resetBtn");
+
+
+function printWord(){
+    alert(inputTxt.value);
+    
+}
+
+function clearWord(){
+    inputTxt.value = "";
+}
+
+subBtn.addEventListener("click", printWord);
+resetBtn.addEventListener("click", clearWord);
