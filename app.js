e736321cdd583c6bbc4969d477f0cc2ba1914cbf
Shlayten <jaouane.mohamed.achraf@gmail.com>
2024-02-24 22:56:52 +0100

    nichts

--- app.js
+++ app.js
@@ -19,3 +19,8 @@ resetBtn.addEventListener("click", clearWord);
 for(let i=0; i<20;i++){
     document.write(i+"Hallo!!!!!!!")
 }
+
+// heute 
+for(let i=0; i<20;i++){
+    document.write(i+"Hallo!!!!!!!")
+}
