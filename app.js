6f711f18f04c9030dd940913f4ce1019ec8a7f44
Shlayten <jaouane.mohamed.achraf@gmail.com>
2024-02-25 22:07:15 +0100

    ...

--- app.js
+++ app.js
@@ -28,22 +28,32 @@
 let zero = 0;
 let counter = 3;
 
-let my = ["Ahmed","Mazero","Elham","Osama","Gamal","Ameer"];
+let my = [ 1, 3, "Ahmed","Mazero","Elham","Osama","Gamal","Ameer" ];
+let onlyNames = [];
+// my = my.slice(zero,++counter).reverse();
+// // console.log(my);
 
-my = my.slice(zero,++counter).reverse();
-// console.log(my);
+// my = my.slice(++zero,--counter);
+// // console.log(my);
 
-my = my.slice(++zero,--counter);
-// console.log(my);
-
-my = my[--zero].slice(zero,--counter)+my[++zero].slice(++zero);
-// console.log(my);
-
-
-my1 = my.slice(-(counter),-(--counter));
-
-my2 = my.slice(-(counter)).toUpperCase();
+// my = my[--zero].slice(zero,--counter)+my[++zero].slice(++zero);
+// // console.log(my);
 
 
-console.log(my1 + my2);
+// my1 = my.slice(-(counter),-(--counter));
 
+// my2 = my.slice(-(counter)).toUpperCase();
+
+
+// console.log(my1 + my2);
+
+
+
+for(let i =0; i < my.length;i++){
+    
+    if( typeof my[i] === "string" ){
+        onlyNames.push(my[i]);
+    }
+}
+
+console.log(onlyNames);
