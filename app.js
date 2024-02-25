2ae3ca685c969ccec17bbfa84a59091376165c58
Shlayten <jaouane.mohamed.achraf@gmail.com>
2024-02-25 13:18:17 +0100

    elzero Array challenge

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
-let inputTxt = document.querySelector(".inputTxt");
-let subBtn = document.querySelector(".subBtn");
-let resetBtn = document.querySelector(".resetBtn");
+// let inputTxt = document.querySelector(".inputTxt");
+// let subBtn = document.querySelector(".subBtn");
+// let resetBtn = document.querySelector(".resetBtn");
 
 
-function printWord(){
-    alert(inputTxt.value);
+// function printWord(){
+//     alert(inputTxt.value);
     
-}
+// }
 
-function clearWord(){
-    inputTxt.value = "";
-}
+// function clearWord(){
+//     inputTxt.value = "";
+// }
 
-subBtn.addEventListener("click", printWord);
-resetBtn.addEventListener("click", clearWord);
+// subBtn.addEventListener("click", printWord);
+// resetBtn.addEventListener("click", clearWord);
 
 
-for(let i=0; i<20;i++){
-    document.write(i+"Hallo!!!!!!!")
-}
+// for(let i=0; i<20;i++){
+//     document.write(i+"Hallo!!!!!!!")
+// }
 
-// heute 
-for(let i=0; i<20;i++){
-    document.write(i+"Hallo!!!!!!!")
-}
+// // heute 
+// for(let i=0; i<20;i++){
+//     document.write(i+"Hallo!!!!!!!")
+// }
+
+let zero = 0;
+let counter = 3;
+
+let my = ["Ahmed","Mazero","Elham","Osama","Gamal","Ameer"];
+
+my = my.slice(zero,++counter).reverse();
+// console.log(my);
+
+my = my.slice(++zero,--counter);
+// console.log(my);
+
+my = my[--zero].slice(zero,--counter)+my[++zero].slice(++zero);
+// console.log(my);
+
+
+my1 = my.slice(-(counter),-(--counter));
+
+my2 = my.slice(-(counter)).toUpperCase();
+
+
+console.log(my1 + my2);
+
